84811dae54c80b0e8700d37e78441efb6f25bf4c
Noah Sussman <noah@Arithromancy.local>
2009-01-09 17:15:16 -0500

    updated documentation now that the gem is functional

--- gemspec.rb
+++ gemspec.rb
@@ -7,11 +7,8 @@ SPEC = Gem::Specification.new do |spec|
   spec.version = "0.0.1"
   spec.summary = "The 3-in-1 Validator: wrapper for Tidy, OnSGMLS and Raakt."
 
-  #spec.add_dependency("open3")
   spec.add_dependency("mechanize")
   spec.add_dependency("raakt")
-  #spec.add_dependency("uri")
-  #spec.add_dependency("net/http")
 
   require 'rake'
 
